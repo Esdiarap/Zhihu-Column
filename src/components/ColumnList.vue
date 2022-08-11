c6c62b925d2adf12dad455d8c6d99d6b65d5e4f6
Esdiarap <esid13122@gmail.com>
2022-08-11 22:33:35 +0800

    ColumnList组件的编写

--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,25 +1,29 @@
 <template>
-  <ul>
-    <li v-for="column in lists" :key="column.id">
-      <img :src="column.avatar" :alt="column.title">
-      <h5>{{column.title}}</h5>
-      <p>{{column.description}}</p>
-      <a href="#">进入专栏</a>
-    </li>
-  </ul>
+  <div class="row">
+    <div v-for="column in columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm">
+        <div class="card-body text-center">
+          <img :src="column.avatar" :alt="column.title" class="rounded-circle border border-light w-25 my-3">
+          <h5 class="card-title">{{column.title}}</h5>
+          <p class="card-text">{{column.description}}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import {PropType} from "vue";
+import {computed, defineComponent, PropType} from "vue";
 // 暴露接口
 export interface ColumnProps {
   id: number,
   title: string,
-  avatar: string,
+  avatar?: string,
   description: string
 }
 
-export default {
+export default defineComponent({
   name: 'ColumnList',
   props: {
     lists: {
@@ -27,8 +31,19 @@ export default {
       required: true
     }
   },
-
-}
+  setup(props) {
+    const columnList = computed(() => props.lists.map(column => {
+      if (!column.avatar) {
+        // 这里不知道怎么把照片转成string，其实就是一个地址而已。。
+        // const jpg = await import('../assets/skadi.jpg')
+      }
+      return column
+    }))
+    return {
+      columnList
+    }
+  }
+})
 </script>
 
 

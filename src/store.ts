245092cfd5261123ae34f13456035e213f8f7bfb
Esdiarap <esid13122@gmail.com>
2022-08-14 21:26:51 +0800

    缓存优化第一部分，先修改columns和posts的数据结构，添加isLoaded和loadedColumnsID，通过这俩来判断是否需要进一步发送请求获取数据

--- src/store.ts
+++ src/store.ts
@@ -54,8 +54,8 @@ interface ListProps<T> {
 
 export interface GlobalDataProps {
     error: GlobalErrorProps
-    columns: ListProps<ColumnProps>
-    posts: ListProps<PostProps>
+    columns: { data: ListProps<ColumnProps>, isLoaded: boolean }
+    posts: { data: ListProps<PostProps>, loadedColumns: string[] }
     user: UserProps,
     loading: false,
     token: string
@@ -73,15 +73,25 @@ const postAndCommit = async (url: string, mutationName: string, commit: Commit, 
     return data
 }
 
-const asyncAndCommit = async (url: string, mutationName: string, commit: Commit, config: AxiosRequestConfig = {method: 'get'}) => {
+const asyncAndCommit = async (
+    url: string,
+    mutationName: string,
+    commit: Commit,
+    config: AxiosRequestConfig = {method: 'get'},
+    extraData?: any
+) => {
     const {data} = await axios(url, config)
-    commit(mutationName, data)
+    if (extraData) {
+        commit(mutationName, {data, extraData})
+    } else {
+        commit(mutationName, data)
+    }
     return data
 }
 const store = createStore<GlobalDataProps>({
     state: {
-        columns: {},
-        posts: {},
+        columns: {data: {}, isLoaded: false},
+        posts: {data: {}, loadedColumns: []},
         user: {isLogin: false},
         loading: false,
         token: localStorage.getItem('token') || '',
@@ -103,19 +113,21 @@ const store = createStore<GlobalDataProps>({
             router.push('/')
         },
         createPost(state, newPost) {
-            state.posts[newPost._id] = newPost
+            state.posts.data[newPost._id] = newPost
         },
         fetchColumns(state, rawData) {
-            state.columns = arrToObj(rawData.data.list)
+            state.columns.data = arrToObj(rawData.data.list)
+            state.columns.isLoaded = true
         },
         fetchColumn(state, rawData) {
-            state.columns[rawData.data._id] = rawData.data
+            state.columns.data[rawData.data._id] = rawData.data
         },
-        fetchPosts(state, rawData) {
-            state.posts = arrToObj(rawData.data.list)
+        fetchPosts(state, {data: rawData, extraData: columnId}) {
+            state.posts.data = {...state.posts.data, ...arrToObj(rawData.data.list)}
+            state.posts.loadedColumns.push(columnId)
         },
         fetchPost(state, rawData) {
-            state.posts[rawData.data._id] = rawData.data // 此时只有一个Post了，posts数组只有一项，但是也要用数组包裹起来。而且此处获取的Post是有Content的
+            state.posts.data[rawData.data._id] = rawData.data // 此时只有一个Post了，posts数组只有一项，但是也要用数组包裹起来。而且此处获取的Post是有Content的
         },
         setLoading(state, status) {
             state.loading = status
@@ -127,21 +139,27 @@ const store = createStore<GlobalDataProps>({
             state.user = {isLogin: true, ...rawData.data}
         },
         updatePost(state, {data}) {
-            state.posts[data._id] = data
+            state.posts.data[data._id] = data
         },
         deletePost(state, {data}) {
-            delete state.posts[data._id]
+            delete state.posts.data[data._id]
         }
     },
     actions: {
-        fetchColumns({commit}) {
-            return getAndCommit('/columns', 'fetchColumns', commit)
+        fetchColumns({state, commit}) {
+            if (!state.columns.isLoaded) { // 如果专栏没有被加载
+                return getAndCommit('/columns', 'fetchColumns', commit)
+            }
         },
-        fetchColumn({commit}, cid) {
-            return getAndCommit(`/columns/${cid}`, 'fetchColumn', commit)
+        fetchColumn({state, commit}, cid) {
+            if (!state.columns.data[cid]) {
+                return getAndCommit(`/columns/${cid}`, 'fetchColumn', commit)
+            }
         },
-        fetchPosts({commit}, cid) {
-            return getAndCommit(`/columns/${cid}/posts`, 'fetchPosts', commit)
+        fetchPosts({state, commit}, cid) {
+            if (!state.posts.loadedColumns.includes(cid)) {
+                return asyncAndCommit(`/columns/${cid}/posts`, 'fetchPosts', commit, {method: 'get'}, cid) // 把columnId带上
+            }
         },
         fetchPost({commit}, pid) {
             return getAndCommit(`/posts/${pid}`, 'fetchPost', commit)
@@ -167,13 +185,10 @@ const store = createStore<GlobalDataProps>({
         }
     },
     getters: {
-        columnNumber(state) {
-            return state.columns.length
-        },
-        getColumnById: state => (id: string) => state.columns[id],
-        getPostsById: state => (cid: string) => objToArr(state.posts).filter(post => post.column === cid),
-        getPostById: state => (pid: string) => state.posts[pid],
-        getColumns: state => objToArr(state.columns)
+        getColumnById: state => (id: string) => state.columns.data[id],
+        getPostsById: state => (cid: string) => objToArr(state.posts.data).filter(post => post.column === cid),
+        getPostById: state => (pid: string) => state.posts.data[pid],
+        getColumns: state => objToArr(state.columns.data)
     }
 })
 
